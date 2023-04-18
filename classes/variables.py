64b02cb9880f76332a6c75a30038109c574cda74
PixelMutation <charlie.e.spencer@gmail.com>
2023-04-18 15:15:56 +0100

    create variables file Variables now referenced by Sympy constants instead of strings. This allows dependencies to easily be substituted

---
+++ classes/variables.py
@@ -0,0 +1,27 @@
+from sympy import symbols
+
+Vin  = symbols("Vin"  )
+Vout = symbols("Vout" )
+Iin  = symbols("Iin"  )
+Iout = symbols("Iout" )
+Zin  = symbols("Zin"  )
+Zout = symbols("Zout" )
+Pin  = symbols("Pin"  )
+Pout = symbols("Pout" )
+Av   = symbols("Av"   )
+Ai   = symbols("Ai"   )
+Ap   = symbols("Ap"   ) 
+
+vars={
+    "Vin"   : Vin,    
+    "Vout"  : Vout,  
+    "Iin"   : Iin,    
+    "Iout"  : Iout,  
+    "Zin"   : Zin,    
+    "Zout"  : Zout,  
+    "Pin"   : Pin,    
+    "Pout"  : Pout,  
+    "Av"    : Av,      
+    "Ai"    : Ai,      
+    "Ap"    : Ap,      
+}
